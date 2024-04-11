b816c125a79755a1c6da8e8f3bf540cc60e47998
jmzhoulab <jmzhoulab@sina.cn>
2024-04-11 22:42:08 +0800

    Auto Update

--- update.sh
+++ update.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-set -e
-
 python src/main.py
 
 git add docs data latest.date
 
-git commit -m "Auto Update"
+git_status=$(git status -s | grep "^[AM]")
 
-git push
+if [ -z "$git_status" ]; then
+    echo "没有发现新增或者变更文件"
+    exit 0
+else
+    echo "有已暂存的变更，准备提交："
+    echo "$git_status"
+    git commit -m "Auto Update"
+    git push
+fi
